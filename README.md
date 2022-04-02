2797509eed3dc1b30ab8d024b4e83fb74bb08f1f
KRSBX <firdaus.sati51@gmail.com>
2022-04-02 19:03:04 +0700

    chore: init repo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # filter-query-parser
+
 Filter Query Parser for Prisma ORM
+
+Using [filter-query-parser](https://github.com/VJD7/filter-query-parser/) as the base
---
+++ test/prismaFqp.test.ts
@@ -0,0 +1,6 @@
+describe('Prisma Filter Query Parser', () => {
+  it.todo('It can accept empty filter');
+  it.todo('It can parse multiple filter');
+  it.todo('It can parse nested filter');
+  it.todo('It can parse not operator');
+});
